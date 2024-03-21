7bf7c105b4b10aabd4ff7b8901eeb398ce1776f0
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-03-21 20:18:57 +0100

    spinner and notification in addetail controller

--- addetail/addetail-controller.js
+++ addetail/addetail-controller.js
@@ -6,13 +6,35 @@ export async function adDetailController(adDetailContainer) {
     const params = new URLSearchParams(window.location.search)
     const adId = params.get('adId');
     if (!adId) {
-      window.location.href = './index.html'
+        window.location.href = './index.html'
     }
   
     try {
-      const ad = await getAdData(adId)
-      adDetailContainer.innerHTML = buildAdDetail(ad)
+        spinner('active')
+        const ad = await getAdData(adId)
+        adDetailContainer.innerHTML = buildAdDetail(ad)
     } catch (error) {
-      // Dispatch notification
+        dispatchError(error)
+    } finally{
+        spinner('inactive')
+    }
+
+    function dispatchError(error) {
+        const event = new CustomEvent('notification-event', {
+            detail: {
+                msg: error,
+                type: 'error'
+              }
+          });
+        adDetailContainer.dispatchEvent(event)
+    }
+
+    function spinner(action) {
+        const event = new CustomEvent('spinner-event', {
+            detail: {
+                action
+            }
+        })
+        adsList.dispatchEvent(event)
     }
 }
