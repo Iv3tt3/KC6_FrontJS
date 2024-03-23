f6a7940d1ede1b44a731762a3472717c13f98085
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-03-23 20:33:53 +0000

    newad validations and token

--- newad.js
+++ newad.js
@@ -2,18 +2,28 @@ import { newAdController } from "./newad/newad-controller.js";
 import { notificationController } from "./notifications/notification-controller.js";
 import { spinnerController } from "./spinner/spinner-controller.js";
 
-const notificationContainer = document.querySelector('.notifications');
-const spinnerContainer = document.querySelector('.spinner')
-const newAdForm = document.querySelector('#newad-form');
 
-newAdForm.addEventListener('notification-event', (event) => {
-    notificationController(notificationContainer, event.detail.msg, event.detail.type)
-    event.stopPropagation();
+document.addEventListener('DOMContentLoaded', () => {
+
+    const token = localStorage.getItem('token');
+  
+    if (!token) {
+      window.location.href = 'index.html'
+    }
+    
+    const notificationContainer = document.querySelector('.notifications');
+    const spinnerContainer = document.querySelector('.spinner')
+    const newAdForm = document.querySelector('#newad-form');
+
+
+    newAdForm.addEventListener('notification-event', (event) => {
+        notificationController(notificationContainer, event.detail.msg, event.detail.type)
+        event.stopPropagation();
+    })
+
+    newAdForm.addEventListener('spinner-event', (event) => {
+        spinnerController(spinnerContainer, event.detail.action)
+        event.stopPropagation();
+    })
+    newAdController(newAdForm)
 })
-
-newAdForm.addEventListener('spinner-event', (event) => {
-    spinnerController(spinnerContainer, event.detail.action)
-    event.stopPropagation();
-})
-
-newAdController(newAdForm)
--- newad/newad-controller.js
+++ newad/newad-controller.js
@@ -9,17 +9,35 @@ export function newAdController (newAdForm) {
         
     })  
 
-    async function handleSubmit() {
+    function handleSubmit() {
+
         const formData = new FormData(newAdForm);
         const adData = {
             name: formData.get("name"),
-            price: formData.get("name"),
-            img: formData.get("img"),
-            type: formData.get("typeAd"),
+            price: formData.get("price"),
+            img: (formData.get("img") === '' ? './images/noimg.png' : formData.get("img")),
+            type: `To ${formData.get("typeAd")}`,
             description: formData.get("description")
         }
 
+        let errors = [];
 
+        if (!priceValid(adData.price)) {
+            errors.push('Not a valid price. Price must be greater than 0 and have a maximum of 2 decimals.')
+        }
+
+        if (!imgValid(adData.img)) {
+            errors.push('Image not valid')
+        }
+
+        showErrors(errors);
+
+        if (errors.length === 0) {
+            sendDataToModel(adData)
+        }
+    }
+
+    async function sendDataToModel(adData){
         try {
             spinner('active')
             await createAd(adData);
@@ -33,6 +51,21 @@ export function newAdController (newAdForm) {
         } finally{
             spinner('inactive')
         }
+    }
+
+
+    function showErrors(errors) {
+        for (const error of errors) {
+            dispatchNotification('error', error)
+        }
+    }
+
+    function priceValid(price) {
+        return new RegExp(/^[1-9]\d{0,2}(?:[.,]\d{3})*(?:,\d[1-9])?$/).test(price)
+    }
+
+    function imgValid(img) {
+        return new RegExp(/.\/images\/\w+\.(jpg|png|jpeg)/).test(img)
     }
 
     function dispatchNotification(type, msg) {
@@ -54,4 +87,4 @@ export function newAdController (newAdForm) {
         newAdForm.dispatchEvent(event)
     }
 
-};
+}
